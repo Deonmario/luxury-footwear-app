05696809531e2b854d5af64effd1ab3c0a34733c
Deonmario <156459842+Deonmario@users.noreply.github.com>
2025-01-31 13:50:15 +0530

    update to have real names

--- lib/pages/wishlist_page.dart
+++ lib/pages/wishlist_page.dart
@@ -3,9 +3,10 @@ import '../layouts/main_layout.dart';
 
 class WishlistPage extends StatelessWidget {
   final List<Map<String, dynamic>> wishlistItems = [
-    {'name': 'Shoe 1', 'price': 150.0, 'image': 'images/shoe1.jpg'},
-    {'name': 'Shoe 2', 'price': 200.0, 'image': 'images/shoe2.jpg'},
-    {'name': 'Shoe 3', 'price': 180.0, 'image': 'images/shoe3.jpg'},
+    {'name': 'Nike Air Max 270', 'price': 150.0, 'image': 'images/shoe1.jpg'},
+    {'name': 'Adidas Ultraboost 22', 'price': 200.0, 'image': 'images/shoe2.jpg'},
+    {'name': 'Puma RS-X', 'price': 180.0, 'image': 'images/shoe3.jpg'},
+    {'name': 'New Balance 574', 'price': 250.0, 'image': 'images/shoe4.jpg'},
   ];
 
   WishlistPage({Key? key}) : super(key: key);
